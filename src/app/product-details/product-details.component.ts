bbbe49aafe9b1d822d5c67b478dc5aa69dd9c689
EdgarAraya <edgar.araya1801@alumnos.ubiobio.cl>
2020-10-09 16:25:11 -0300

    1 file modified, 1 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 import {products} from '../products'
 @Component({
@@ -8,10 +8,17 @@ import {products} from '../products'
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+  product;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,) { 
+    
+  }
 
   ngOnInit() {
-  }
+    this.route.paramMap.subscribe(params =>{
+
+      this.product= products[+params.get('productId')];
+    }
+)};
 
 }
